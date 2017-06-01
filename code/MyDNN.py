56b214a84cd700b0419ba719062929ad69bbe008
duxuhao <duxuhao@gmail.com>
2017-06-02 02:01:46 +1000

    start adding features

--- code/MyDNN.py
+++ code/MyDNN.py
@@ -1,5 +1,4 @@
 import time
-import itertools
 import tensorflow as tf
 import pandas as pd
 import numpy as np
@@ -8,36 +7,75 @@ from multiprocessing import Pool
 from tensorflow.contrib.learn.python.learn.estimators import model_fn as model_fn_lib
 
 label = 'logerror'
-features = ['airconditioningtypeid', 'architecturalstyletypeid','basementsqft', 'bathroomcnt', 'bedroomcnt', 'buildingclasstypeid', 'buildingqualitytypeid']
+labelname = "../data/train_2016.csv"
+featuresname = "../data/properties_2016.csv"
+subname = "../data/sample_submission.csv"
+features = ['airconditioningtypeid', 
+            'architecturalstyletypeid',
+            'basementsqft', 
+            'bathroomcnt', 
+            'bedroomcnt', 
+            'buildingclasstypeid',  
+            'buildingqualitytypeid',
+            'assessmentyear',
+            'lotsizesquarefeet',
+            #'finishedsquarefeet12',
+            'finishedsquarefeet15',
+            'fullbathcnt',
+            'numberofstories',
+            #'roomcnt',
+            #'taxvaluedollarcnt',
+            #'taxdelinquencyyear',
+            #'taxdelinquencyflag',
+            #'regionidcounty',
+            'regionidcity',
+            #'regionidzip',
+            #'regionidneighborhood',
+            ]
+
+OneHotFeature = [[['regionidcity'], 'region']]
 
 def convertdataset(df):
+    if 'transactiondate' in df.columns:
+        df.drop('transactiondate', inplace = True, axis=1)
+    print df.shape
     Dataset = collections.namedtuple('Dataset',['data','target'])
-    return Dataset(data=np.array(df[features]),target=np.array(df[label]))
+    return Dataset(data=np.array(df.ix[:,df.columns != label]),target=np.array(df[label]))
 
 def preparedataframe():
-    labelname = "../data/train_2016.csv"
-    featuresname = "../data/properties_2016.csv"
-    subname = "../data/sample_submission.csv"
-
     labeldf = pd.read_csv(labelname)
     featuresdf = pd.read_csv(featuresname)
+    featuresdf = featuresdf[features + ['parcelid']]
     sub = pd.read_csv(subname)
     sub.rename(columns={'ParcelId': 'parcelid'}, inplace = True)
     date = range(len(np.unique(labeldf.transactiondate)))
     labeldf.transactiondate.replace(np.unique(labeldf.transactiondate), date, inplace = True)
-
+    '''
+    for OHF in OneHotFeature[:1]:
+        temp = featuresdf[OHF[0]].drop_duplicates()
+        print temp.shape
+        for onh in range(temp.shape[0]):
+            temp['{}_{}'.format(OHF[1],onh)] = 0
+            temp.loc[onh, '{}_{}'.format(OHF[1],onh)] = 1    
+        featuresdf = featuresdf.merge(temp, on = OHF[0], how = 'left')        
+    '''
     df = labeldf.merge(featuresdf, on = 'parcelid', how = 'left')
-    pre = sub.merge(featuresdf, on = 'parcelid', how = 'left')
+    pre = sub[['parcelid']].merge(featuresdf, on = 'parcelid', how = 'left')
     df.fillna(0.0,inplace = True)
     pre.fillna(0.0,inplace = True)
     pre[label] = np.nan
     return df, pre
 
 
-def pred(regressor, sub, pre, filename):
-    pre['logerror'] = np.nan
-    y = regressor.predict(input_fn=lambda: input_fn(pre))
-    prediction = list(itertools.islice(y, sub.shape[0]))
+def pred(regressor, pre, filename):
+    sub = pd.read_csv(subname)
+    pre[label] = np.nan
+    def input_predict_fn():
+        newpre = convertdataset(pre)
+        return tf.constant(newpre.data), tf.constant(newpre.target)
+
+    y = regressor.predict(input_fn=input_predict_fn, as_iterable=True)
+    prediction = [i[label] for i in y]
     for i in sub.columns[1:]:
         sub[i] = prediction
     sub.rename(columns={'parcelid': 'ParcelId'}, inplace = True)
@@ -45,18 +83,24 @@ def pred(regressor, sub, pre, filename):
 
 
 def model_fn(features, targets, mode, params):
-    first_hidden_layer = tf.contrib.layers.relu(features, 10)
+    first_hidden_layer = tf.contrib.layers.relu(features, 16)
     second_hidden_layer = tf.contrib.layers.relu(first_hidden_layer,10)
+    #third_hidden_layer = tf.contrib.layers.relu(second_hidden_layer,5)
     output_layer = tf.contrib.layers.linear(second_hidden_layer, 1)
+
     predictions = tf.reshape(output_layer, [-1])
-    prediction_dict = {'logerror': predictions}
+    prediction_dict = {label: predictions}
+
     loss = tf.losses.absolute_difference(targets, predictions)
+
     eval_metric_ops = {'mae': tf.metrics.mean_absolute_error(tf.cast(targets, tf.float64), predictions)}
+
     train_op = tf.contrib.layers.optimize_loss(
                                               loss=loss,
                                               global_step=tf.contrib.framework.get_global_step(),
                                               learning_rate=params['learning_rate'],
                                               optimizer='SGD')
+
     return model_fn_lib.ModelFnOps(
                                   mode=mode,
                                   predictions=prediction_dict,
@@ -70,26 +114,26 @@ def main():
     LEARNING_RATE = 0.1
     model_params = {"learning_rate": LEARNING_RATE}
     Mynn = tf.contrib.learn.Estimator(model_fn = model_fn, params=model_params)
-    seperatetime = 10
-
+    traintime = 300
+    testtime = 300
     def input_train_fn():
-        train = convertdataset(df[df.transactiondate < seperatetime])
+        train = convertdataset(df[df.transactiondate < traintime])
         return tf.constant(train.data), tf.constant(train.target)
-
+    start = time.time()
     Mynn.fit(input_fn=input_train_fn, steps=5000)
 
     def input_test_fn():
-        test = convertdataset(df[df.transactiondate >= seperatetime])
+        test = convertdataset(df[df.transactiondate >= testtime])
         return tf.constant(test.data), tf.constant(test.target)
 
-    ev = Mynn.evaluate(input_fn=input_train_fn)
-    print("Mean Absolute Error: {}".format(ev['mae']))
-    '''
-    predictions = Mynn.predict(x=predic.data, as_iterable=True)
-    for i, p in enumerate(predictions):
-        print("Prediction %s: %s" % (i + 1, p["logloss"]))
-    '''
-    #pred(regressor, sub,pre, 'test.csv')
-
+    #ev = Mynn.evaluate(input_fn=input_train_fn)
+    #print("Mean Absolute Error: {}".format(ev['mae']))
+    ev = Mynn.predict(input_fn=input_test_fn, as_iterable=True)
+    evalvalue = [i[label] for i in ev]
+    
+    ev = np.mean(np.abs(np.array(evalvalue) - np.array(df[df.transactiondate >= testtime][label])))
+    print(ev)
+    pred(Mynn, predic, 'test1010_morefea.csv')
+    print('used time: {} s'.format(time.time()-start))
 if __name__ == "__main__":
     main()
